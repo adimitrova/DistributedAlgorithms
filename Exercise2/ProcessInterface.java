ac8d0c2e4a71328f642356d0791b55012587deb0
adimitrova <a.dimitrovaa@gmail.com>
2017-11-27 21:47:23 +0100

    begin coding ex2, but not exactly following the pseudo code, but following my own understanding

---
+++ Exercise2/ProcessInterface.java
@@ -0,0 +1,26 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * Interface for the process in order for the client what it can expect from the process.
+ * 
+ * @author Anelia Dimitrova (4501667) & Laurens Weijs (4503813)
+ * @version	27.11.17
+ */
+public interface ProcessInterface extends Remote {
+    /**
+     * send this process' id and nnid to the next process
+     * @param own id (int)
+     * @param own nnid (int)
+     * @throws RemoteException
+     */
+    public void send(int ownID, int ownNNID) throws RemoteException;
+
+    /**
+     * Receiving the left neighbour's ID and NNID
+     * @param leftNeighbNNID (int)
+     * @param leftNeighbID (int)
+     * @throws RemoteException
+     */
+    public void receive(int leftNeighbID, int leftNeighbNNID) throws RemoteException;
+}
